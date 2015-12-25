0c82d1dc5d27169d32b537aa157941f7d90cbb50
Juho Teperi <juho.teperi@iki.fi>
2015-12-25 17:20:00 +0200

    Fix bad require

--- test/sass4clj/webjars_test.clj
+++ test/sass4clj/webjars_test.clj
@@ -1,6 +1,6 @@
 (ns sass4clj.webjars-test
   (:require [clojure.test :refer :all]
-            [less4clj.webjars :as webjars]))
+            [sass4clj.webjars :as webjars]))
 
 (deftest asset-map-test
   (is (contains? (webjars/asset-map) "bootstrap/less/bootstrap.less")))
