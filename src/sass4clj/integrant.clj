6cd198f614fa24c2cfd94d22a9c25df48bd86dd4
Juho Teperi <juho.teperi@iki.fi>
2019-01-04 15:04:03 +0200

    Namespaced keys for integrant

--- src/sass4clj/integrant.clj
+++ src/sass4clj/integrant.clj
@@ -2,16 +2,16 @@
   (:require [sass4clj.api :as api]
             [integrant.core :as ig]))
 
-(defmethod ig/init-key :sass4clj [_ options]
+(defmethod ig/init-key ::sass4clj [_ options]
   (api/start options))
 
-(defmethod ig/halt-key! :sass4clj [this options]
+(defmethod ig/halt-key! ::sass4clj [this options]
   (api/stop this))
 
-(defmethod ig/suspend-key! :sass4clj [this options]
+(defmethod ig/suspend-key! ::sass4clj [this options]
   nil)
 
-(defmethod ig/resume-key :sass4clj [key opts old-opts old-impl]
+(defmethod ig/resume-key ::sass4clj [key opts old-opts old-impl]
   (if (= opts old-opts)
     old-impl
     (do
