531e1f5d723b1aba123e87db5261b869599e3e4e
Juho Teperi <juho.teperi@iki.fi>
2019-08-20 12:15:15 +0300

    Add main options to read config from an edn file

--- src/sass4clj/main.clj
+++ src/sass4clj/main.clj
@@ -3,7 +3,8 @@
             [clojure.java.io :as io]
             [clojure.tools.cli :as cli]
             [clojure.string :as str]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [clojure.edn :as edn]))
 
 (def cli-opts
   [["-h" "--help"]
@@ -11,7 +12,7 @@
    ["-t" "--target-path TARGET" "The path where CSS files are written to"
     :default "target"]
    ["-s" "--source-map" "Enable source-maps for compiled CSS"]
-   [nil "--output-style STYLE" "Select output style, available styles are nested, compact, expanded and compressed."
+   [nil "--output-style STYLE" "Select output style, available styles are nested, compact, expanded and compressed"
     :parse-fn (fn [s] (keyword s))
     :validate [#{:nested :compact :expanded :compressed} "Invalid value"]]
    ["-v" "--verbosity LEVEL" "Set verbosity level, valid values are 1 and 2."
@@ -21,7 +22,8 @@
    [nil "--source-paths PATHS" "List of LESS source paths, comma separated"
     :default ["src"]
     :parse-fn (fn [x]
-                (str/split x #","))]])
+                (str/split x #","))]
+   ["-c" "--config PATH" "EDN file to read config options from"]])
 
 (defn help-text [options-summary]
 (str "{less} CSS compiler.
@@ -35,11 +37,19 @@ code should be at path `{source-path}/public/css/style.main.less`.
 If you are seeing SLF4J warnings, check https://github.com/Deraen/sass4clj#log-configuration
 
 Options:
-" options-summary))
+" options-summary
+"
+
+Config file options are merged over the default options, before CLI options."))
 
 (defn -main [& args]
-  (let [{:keys [options summary errors]} (cli/parse-opts args cli-opts)
-        {:keys [help]} options]
+  (let [{:keys [options summary errors]} (cli/parse-opts args cli-opts :no-defaults true)
+        {:keys [help]} options
+        config-file (if (:config options)
+                      (edn/read-string (slurp (io/file (:config options)))))
+        options (merge (cli/get-default-options cli-opts)
+                       config-file
+                       options)]
     (cond
       errors (println (str/join "\n" errors))
       help (println (help-text summary))
